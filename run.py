979c700d7384e0b28d79a0cc471ecd39e403000c
Maurice Günder <mauriceguender@yahoo.de>
2020-05-08 21:03:16 +0200

    fixed path for invoking from other working directories

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
+import os
 import json
 from weathercrawler import WeatherCrawler
 
 
 if __name__ == '__main__':
-    with open('config.json', 'r') as f:
+    with open(os.path.join(os.path.dirname(__file__), 'config.json'), 'r') as f:
         config = json.load(f)
 
     crawler = WeatherCrawler(**config)
